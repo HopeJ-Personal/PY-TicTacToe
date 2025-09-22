8734fd30b695a3327c9c1317b5d332db55a63d08
HopeJ-Personal <HopeJ-Personal@gmail.com>
2025-09-21 23:16:34 -0700

    Updated test calls for table generation, added more icon variables/variations to test size changes and the actual designing of icons for tiktactoe

--- main.py
+++ main.py
@@ -27,10 +27,31 @@ table.test("abc")
 #table.generate_table(3, 3)
 #table.generate_table(3, 3, 10, 5, 1, "This is a sample string to demonstrate the table generation functionality.")
 #table.generate(3,2)
-table.generate(3, 3, 2, 2, 1, "xoooxooox")
-ico_x = "x   x  x  x   x"
-ico_o = "  o  o   o  o  "
+#table.generate(3, 3, 2, 2, 1, "xoooxooox")
+#ico_x = "x   x  x  x   x"
+#ico_o = "  o  o   o  o  "
+
+#ico_x = '\\   /  x  /   \\'
+#ico_x = '╲  /  x  /  ╲'
+#ico_x = "╔═╗║  ║╚═══╝"
 
 #x   x  x  x   x
 #  o  o   o  o  
-table.generate(4, 4, 5, 15, 1, f"{ico_x}{ico_o}{ico_x}{ico_x}{ico_o}{ico_o}{ico_o}{ico_o}{ico_o}{ico_o}{ico_x}{ico_x}")
+#table.generate(4, 4, 5, 15, 1, f"{ico_x}{ico_o}{ico_x}{ico_x}{ico_o}{ico_o}{ico_o}{ico_o}{ico_o}{ico_o}{ico_x}{ico_x}")
+#table.generate(3, 3, 5, 15, 1, f"{ico_x}{ico_o}{ico_x}{ico_x}{ico_o}{ico_o}{ico_o}{ico_o}{ico_o}")
+#{ico_x}
+
+ico_x = "\\ / x / \\"
+ico_o = "╔═╗║ ║╚═╝"
+table.generate(3, 3, 3, 9, 1, f"{ico_x}{ico_o}{ico_x}{ico_x}{ico_o}{ico_o}{ico_o}{ico_o}{ico_o}")
+table.generate(3, 3, 3, 9, 1, f"{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}")
+
+ico_x = "\\   /  x  /   \\"
+ico_o = "╔═══╗║   ║╚═══╝"
+table.generate(3, 3, 5, 15, 1, f"{ico_x}{ico_o}{ico_x}{ico_x}{ico_o}{ico_o}{ico_o}{ico_o}{ico_o}")
+table.generate(3, 3, 5, 15, 1, f"{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}")
+
+ico_x = "x"
+ico_o = "o"
+table.generate(3, 3, 1, 1, 1, f"{ico_x}{ico_o}{ico_x}{ico_x}{ico_o}{ico_o}{ico_o}{ico_o}{ico_o}")
+table.generate(3, 3, 1, 1, 1, f"{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}{ico_x}")
