f824bb0411a1e476a2bdf5e3d671fd6a0a1c419d
HopeJ-Personal <HopeJ-Personal@gmail.com>
2025-09-21 17:48:33 -0700

    U2

--- library/table/index.py
+++ library/table/index.py
@@ -1,50 +1,26 @@
-print("index for table")
+## Usefull Links:
+# JSON Formatter: https://jsonformatter.com/
+# UUID V4 Generator: https://www.uuidtools.com/v4
+print("index for tables")
 
-# example from my external border thingy just so i remember how
-class border_class:
-    def create():
-        print("test")
-    
-    def test(self, param):
-        print(f"test subcommand ran with {param}")
-    
-    def set(self, param):
-        print("test")
-    
-    def style(self, param):
-        print("test")
+import sys
+import os
 
 class table_class:
-    def test():
-        print("test")
-    def generate1(width, height, top, left, bottom, right, cell_width, cell_strings):
-            # t = "ABC"
-            # l = "123"
-            # b = ""
-            # l = ""
-            # strings = "xy","zo"
-            # Example table.generate(3, 3, t, l, b, r, 1, strings)
-            # > |   | A | B | C |
-            # > | 1 |
-            print("test")
-    def generate(horizontal, vertical, cell_width, input_width, padding, string, highlight_cells=None, highlight_color=GREEN):
-        #
-        # horizontal: Number of columns
-        # vertical: Number of rows
-        # cell_width: Character width inside each cell
-        # input_width: How many characters to split from the string before putting in cells
-        # padding: Spaces on each side of the text
-        # string: Text to display
-        # highlight_cells: List of (row, col)
-        # highlight_color: Color
-        
-        # Future highlighting idea, idk
-        #   - Optional Parameter support
-        if highlight_cells is None:
-            highlight_cells = []
-        
-        # Split Text
-        #   * string="xxooxx", input_width=2 ---> ["xx","oo","xx"]
-        chunks = textwrap.wrap(string, width=input_width)
-        # printing to see progress live
-        print(chunks)
+    #def test():
+    #def test(self, param):
+    def test(param):
+        print(f"test subcommand ran with {param}")
+        print("this is a test message inside of the index.py of the 'Table library")
+    
+    # Utilities: Timer
+    #def timer_start():
+    #def timer_end()
+    #def timer_calculate()
+    
+    # Utilities: Validation
+    #def validate_file():
+    #def validate_folder():
+    #def validate_input():
+    #def validate_length
+    #def validate_output():
